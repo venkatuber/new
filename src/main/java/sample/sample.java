a543c1706244c32bd2dca9f3f4625e64202c835b
puppy <puppy@DESKTOP-5JRTIS3>
2022-04-22 11:16:49 +0530

    New commit

--- src/main/java/sample/sample.java
+++ src/main/java/sample/sample.java
@@ -2,6 +2,6 @@ package sample;
 
 public class sample {
 	 public static void main(String[] args) {
-	     System.out.println("Hello, World Venkat!!!"); 
+	     System.out.println("Hello, World Ravi"); 
 	 }
 }
